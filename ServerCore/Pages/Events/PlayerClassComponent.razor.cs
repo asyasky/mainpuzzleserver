25ea95495c9ccecc2d9423f1c9cc440ecb3e5f53
Megan Quinn <asyasky@gmail.com>
2025-03-12 17:35:56 -0700

    Add UI pages

---
+++ ServerCore/Pages/Events/PlayerClassComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using ServerCore.DataModel;
+
+namespace ServerCore.Pages.Events
+{
+    public partial class PlayerClassComponent
+    {
+        [Parameter]
+        public int EventId { get; set; }
+
+        [Inject]
+        public PuzzleServerContext _context { get; set; }
+
+        List<PlayerClass> PlayerClasses = new List<PlayerClass>();
+    }
+}
