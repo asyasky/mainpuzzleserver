6e837df49a08ada45196cc59cc15a109505fc6e8
Megan Quinn <asyasky@gmail.com>
2025-03-23 16:02:11 -0700

    Add temporary player classes

--- ServerCore/Pages/Components/PlayerClassSelectComponent.razor.cs
+++ ServerCore/Pages/Components/PlayerClassSelectComponent.razor.cs
@@ -5,6 +5,7 @@ using ServerCore.DataModel;
 using ServerCore.Helpers;
 using ServerCore.ModelBases;
 using System;
+using System.Diagnostics.Eventing.Reader;
 
 namespace ServerCore.Pages.Components
 {
@@ -19,8 +20,14 @@ namespace ServerCore.Pages.Components
         public TeamMembers CurrentTeamMember { get; set; }
 
         [Parameter]
+        public EventRole CurrentUserEventRole { get; set; }
+
+        [Parameter]
         public int EventId { get; set; }
         Event Event { get; set; }
+
+        [Parameter]
+        public bool IsTempClass { get; set; }
 
         [Inject]
         PuzzleServerContext context { get; set; }
@@ -29,17 +36,35 @@ namespace ServerCore.Pages.Components
         {
             Event = await context.Events.FindAsync(EventId);
             CurrentTeamMember = await UserEventHelper.GetTeamMemberForPlayer(context, Event, UserId);
-            AvailablePlayerClasses = await TeamHelper.GetAvailableClasses(context, EventId, CurrentTeamMember.Team.ID);
 
-            // TODO: Add admin override for all player classes (don't filter to available)
+            if(IsTempClass)
+            {
+                AvailablePlayerClasses = await TeamHelper.GetAllPlayerClassesSorted(context, EventId);
+            }
+            else
+            {
+                // This will get unassigned classes for a player or all classes for an admin
+                // Admin UI currently shows the player's current class twice, that's a known bug and can be safely ignored
+                AvailablePlayerClasses = await TeamHelper.GetAvailablePlayerClassesSorted(context, EventId, CurrentUserEventRole, CurrentTeamMember.Team.ID);
+            }
+
             await base.OnParametersSetAsync();
         }
 
         private async Task UpdatePlayerClassID()
         {
-            CurrentTeamMember.Class = await UserEventHelper.GetPlayerClassFromID(context, SelectedPlayerClassID);
-            await context.SaveChangesAsync();
-            AvailablePlayerClasses = await TeamHelper.GetAvailableClasses(context, EventId, CurrentTeamMember.Team.ID);
+            // Update the database with the new PlayerClass, if it's "Unassigned" then set it to null
+            await TeamHelper.SetPlayerClass(context, Event, CurrentUserEventRole, CurrentTeamMember, SelectedPlayerClassID, IsTempClass);
+
+            if (IsTempClass)
+            {
+                AvailablePlayerClasses = await TeamHelper.GetAllPlayerClassesSorted(context, EventId);
+            }
+            else
+            {
+                // Get the new list of unassigned PlayerClasses
+                AvailablePlayerClasses = await TeamHelper.GetAvailablePlayerClassesSorted(context, EventId, CurrentUserEventRole, CurrentTeamMember.Team.ID);
+            }
         }
     }
 }
