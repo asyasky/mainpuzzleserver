d77e8c96ad8ea31e4029737783712121e886d545
Megan Quinn <asyasky@gmail.com>
2026-07-06 18:01:23 -0700

    Mid change all broken

--- ServerCore/Pages/Events/LiveEventManagement.cshtml.cs
+++ ServerCore/Pages/Events/LiveEventManagement.cshtml.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
 using ServerCore.DataModel;
 using ServerCore.ModelBases;
+using Microsoft.AspNetCore.Components.QuickGrid;
 
 namespace ServerCore.Pages.Events
 {
     [Authorize(Policy = "IsEventAdmin")]
     public class LiveEventManagementModel : EventSpecificPageModel
     {
+        [Inject]
+        PuzzleServerContext PuzzleServerContext { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
         public LiveEventManagementModel(PuzzleServerContext serverContext, UserManager<IdentityUser> manager) : base(serverContext, manager)
         {
         }
